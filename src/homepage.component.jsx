8492e1c403fc16ed927316e8b0a3812bb26fcfbc
2magpies <daphnemintz@yahoo.com>
2020-05-03 22:49:01 -0400

    created react app with homepage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="tbd1">
+      <div className="tbd2">
+        <div className="tbd3">
+          <h1 className="tbd4">clouds</h1>
+          <span className="tbd5"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
